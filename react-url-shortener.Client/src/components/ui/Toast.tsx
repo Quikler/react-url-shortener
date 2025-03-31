f8eadf1a66f1e4f5d2df959f1baa393d466b1d4e
Quikler <romuchbinyk228@gmail.com>
2025-03-31 23:42:54 +0300

    refactor(toast): add onTransitionEnd handler to Toast, add handler to handle toast's visivility change in useToast. - Add onTransitionEnd handler to Toast with onVisibilityChange wrapper to lis - Add handler of onVisibilityChange to handle toast's visibility change.

--- react-url-shortener.Client/src/components/ui/Toast.tsx
+++ react-url-shortener.Client/src/components/ui/Toast.tsx
@@ -1,4 +1,3 @@
-import { twMerge } from "tailwind-merge";
 import CheckIcon from "../svgr/CheckIcon";
 import Close from "../svgr/Close";
 import ErrorIcon from "../svgr/ErrorIcon";
@@ -6,12 +5,13 @@ import WarningIcon from "../svgr/WarningIcon";
 
 export type ToastType = "success" | "danger" | "warning";
 
-export type ToastProps = React.HTMLAttributes<HTMLDivElement> & {
+export type ToastProps = {
   message?: string | null;
   type: ToastType;
   isVisible: boolean;
   onClose: () => void;
   transitionDuration: number;
+  onVisibilityChange?: (isVisible: boolean) => void;
 };
 
 const GetToastIcon = (type: ToastType): JSX.Element => {
@@ -26,36 +26,27 @@ const GetToastIcon = (type: ToastType): JSX.Element => {
 };
 
 const Toast = ({
-  className,
   type,
   message,
   isVisible,
   onClose,
   transitionDuration = 1000,
-  ...rest
-}: ToastProps) => {
-
-  return (
-    <div style={{ transitionDuration: `${transitionDuration}ms` }} className={twMerge(
-      `bg-gradient-to-r from-gray-700 to-gray-900 z-[999] flex items-center w-full max-w-xs p-4 rounded-lg shadow-sm text-gray-400 transition-opacity ${isVisible ? "opacity-100" : "opacity-0"
-      }`,
-      className
-    )}
-      role="alert"
-      {...rest}
+  onVisibilityChange,
+}: ToastProps) => (
+  <div onTransitionEnd={() => onVisibilityChange?.(isVisible)} style={{ transitionDuration: `${transitionDuration}ms`, opacity: isVisible ? 1 : 0 }} className="bg-gradient-to-r from-gray-700 to-gray-900 z-[999] flex items-center w-full max-w-xs p-4 rounded-lg shadow-sm text-gray-400 transition-opacity"
+    role="alert"
+  >
+    {GetToastIcon(type)}
+    <div className="ms-3 mr-4 text-sm font-normal break-words">{message}</div>
+    <button
+      onClick={onClose}
+      type="button"
+      className="ms-auto -mx-1.5 -my-1.5 rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8 text-gray-500 hover:text-white cursor-pointer"
+      aria-label="Close"
     >
-      {GetToastIcon(type)}
-      <div className="ms-3 mr-4 text-sm font-normal break-words">{message}</div>
-      <button
-        onClick={onClose}
-        type="button"
-        className="ms-auto -mx-1.5 -my-1.5 rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8 text-gray-500 hover:text-white cursor-pointer"
-        aria-label="Close"
-      >
-        <Close stroke="2" className="fill-gray-400" />
-      </button>
-    </div>
-  );
-};
+      <Close stroke="2" className="fill-gray-400" />
+    </button>
+  </div>
+);
 
 export default Toast;
--- react-url-shortener.Client/src/hooks/useToast.tsx
+++ react-url-shortener.Client/src/hooks/useToast.tsx
@@ -18,7 +18,6 @@ type ToastPropsExtended = ToastProps & {
 };
 
 export const ToastContextProvider = ({ children }: ToastContextProviderProps) => {
-  console.count("Render");
   const TOAST_TRANSITION_DURATION = 1000;
   const TOAST_DISPLAY_LIFETIME = 2000;
 
@@ -26,64 +25,40 @@ export const ToastContextProvider = ({ children }: ToastContextProviderProps) =>
 
   const setToast = (msg: string, type: ToastType) => {
     const toastId = generateUUID();
-    console.log("Setting toast:", toastId);
     const newToastProps: ToastPropsExtended = {
       message: msg,
       type: type,
       isVisible: false,
-      onClose: () => {
-        setToastsProps(prevToasts =>
-          prevToasts.map(t =>
-            t.toastId === toastId ? { ...t, isVisible: false } : t
-          )
-        );
-
-        makeToastCompletelyInvisibleAfterTransitionDuration(toastId);
-      },
+      onClose: () => toggleIsVisible(toastId, false),
       transitionDuration: TOAST_TRANSITION_DURATION,
       toastId: toastId,
       completelyInvisible: false,
+      onVisibilityChange: (isVisible: boolean) => {
+        if (isVisible) {
+          setTimeout(() => toggleIsVisible(toastId, false), TOAST_DISPLAY_LIFETIME);
+        } else {
+          setToastsProps(prevToasts =>
+            prevToasts.map(t =>
+              t.toastId === toastId ? { ...t, completelyInvisible: true } : t
+            )
+          );
+        }
+      }
     };
 
     // Add the new toast
     setToastsProps(prevToasts => [...prevToasts, newToastProps]);
 
     // Make it visible shortly after
-    setTimeout(() => {
-      console.log("Visibility true on toast:", toastId);
-      setToastsProps(prevToasts =>
-        prevToasts.map(t =>
-          t.toastId === toastId ? { ...t, isVisible: true } : t
-        )
-      );
-    }, 10);
-
-    fadeOutToastAfterDisplayLifetime(toastId);
+    setTimeout(() => toggleIsVisible(toastId, true), 10);
   };
 
-  function fadeOutToastAfterDisplayLifetime(toastId: string) {
-    // Hide toast after 2 seconds
-    setTimeout(() => {
-      console.log("Visibility false on toast:", toastId);
-      setToastsProps(prevToasts =>
-        prevToasts.map(t =>
-          t.toastId === toastId ? { ...t, isVisible: false } : t
-        )
-      );
-
-      makeToastCompletelyInvisibleAfterTransitionDuration(toastId);
-    }, TOAST_DISPLAY_LIFETIME);
-  }
-
-  function makeToastCompletelyInvisibleAfterTransitionDuration(toastId: string) {
-    setTimeout(() => {
-      console.log("Transition pass on toast (completely invisible):", toastId);
-      setToastsProps(prevToasts =>
-        prevToasts.map(t =>
-          t.toastId === toastId ? { ...t, completelyInvisible: true } : t
-        )
-      );
-    }, TOAST_TRANSITION_DURATION); // Matches transition duration
+  function toggleIsVisible(toastId: string, visibility: boolean) {
+    setToastsProps(prevToasts =>
+      prevToasts.map(t =>
+        t.toastId === toastId ? { ...t, isVisible: visibility } : t
+      )
+    );
   }
 
   useEffect(() => {
@@ -107,11 +82,24 @@ export const ToastContextProvider = ({ children }: ToastContextProviderProps) =>
 
   return (
     <ToastContext.Provider value={value}>
-      {toastsProps.length !== 0 && <ul className="fixed top-24 right-12 flex flex-col gap-2">
-        {toastsProps.map((value, index) => {
-          return !value.completelyInvisible && <li key={index}><Toast type={value.type} message={value.message} isVisible={value.isVisible} onClose={value.onClose} transitionDuration={value.transitionDuration} /></li>
-        })}
-      </ul>}
+      {toastsProps.length !== 0 &&
+        <ul className="fixed top-24 right-12 flex flex-col gap-2">
+          {toastsProps.map((value) => (
+            !value.completelyInvisible && (
+              <li key={value.toastId}>
+                <Toast
+                  type={value.type}
+                  message={value.message}
+                  isVisible={value.isVisible}
+                  onClose={value.onClose}
+                  transitionDuration={value.transitionDuration}
+                  onVisibilityChange={value.onVisibilityChange}
+                />
+              </li>
+            )
+          ))}
+        </ul>
+      }
       {children}
     </ToastContext.Provider>
   )
